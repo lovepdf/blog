d25e3cca781d0320384e7f15f402ce1058315ff9
binbin <closed66@163.com>
2017-08-15 16:31:03 +0800

    xinde

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,10 @@ from django.conf.urls import patterns,url,include
 from . import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.post_list)
+    url(r'^$', views.post_list),   # 首页
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),  # 文章详情页
+    url(r'^post/new/$', views.post_new, name='post_new'),
+url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 )
 
 
