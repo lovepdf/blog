65890ddba1ee3abb88ad2c8bdd074e28f62fafd3
binbin <closed66@163.com>
2017-08-16 17:16:09 +0800

    fixed

--- blog/urls.py
+++ blog/urls.py
@@ -2,11 +2,14 @@
 from django.conf.urls import patterns,url,include
 from . import views
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', views.post_list),   # 首页
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),  # 文章详情页
     url(r'^post/new/$', views.post_new, name='post_new'),
-url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
-)
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),  # 编辑按钮
+    url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),   # 草稿向
+    url(r'^post/(?P<pk>[0-9]+)/publish/$', views.post_publish, name='post_publish'),  # 发布功能
+    url(r'^post/(?P<pk>[0-9]+)/remove/$', views.post_remove, name='post_remove'),
+    ]
 
 
